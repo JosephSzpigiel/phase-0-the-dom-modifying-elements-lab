bc618893f5cee71e97883716be2f4a223d5a8d40
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-09-13 11:41:26 -0400

    DONE: Lab complete

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const main = document.getElementById("main");
-main.remove();
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
-newHeader.textContent = "Joseph is the champion";
-ReportBody.append(newHeader);
+document.querySelector('#main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.textContent = "Joseph is the champion"
